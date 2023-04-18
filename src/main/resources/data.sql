0d186cc0a94352452230b088bbdc1248ee6b8da0
dmaresza <daniel.a.mares.z@gmail.com>
2023-04-18 16:06:38 -0400

    Added data.sql file to populate db table on application start

---
+++ src/main/resources/data.sql
@@ -0,0 +1,14 @@
+/* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/SQLTemplate.sql to edit this template
+ */
+/**
+ * Author:  danie
+ * Created: Apr 18, 2023
+ */
+
+insert into task (id, priority, description, date) values
+(1, 2, 'Pay bills', '2023-04-30'),
+(2, 3, 'Call Kychelle', '2023-04-18'),
+(3, 1, 'Develop new feature in my app', '2023-05-31'),
+(4, 2, 'Prepare for presentation', '2023-04-24');
