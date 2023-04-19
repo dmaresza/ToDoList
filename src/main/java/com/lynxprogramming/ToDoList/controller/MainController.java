ebdae243913b387466a73ac820cf1f962c114533
dmaresza <daniel.a.mares.z@gmail.com>
2023-04-18 20:20:23 -0400

    Added CRUD operations for adding and removing tasks

--- src/main/java/com/lynxprogramming/ToDoList/controller/MainController.java
+++ src/main/java/com/lynxprogramming/ToDoList/controller/MainController.java
@@ -6,12 +6,13 @@ package com.lynxprogramming.ToDoList.controller;
 
 import com.lynxprogramming.ToDoList.model.Task;
 import com.lynxprogramming.ToDoList.repository.TaskRepository;
-import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -27,15 +28,25 @@ public class MainController {
     @RequestMapping("/")
     public String getTasks(Model model) {
         
-        /*
-        List<Task> tasks = new ArrayList<>();
-        tasks.add(new Task(1, 2, "Pay bills", LocalDate.of(2023, 4, 30)));
-        tasks.add(new Task(2, 3, "Call Kychelle", LocalDate.of(2023, 04, 18)));
-        tasks.add(new Task(3, 1, "Develop new feature in my app", LocalDate.of(2023, 05, 31)));
-        tasks.add(new Task(4, 2, "Prepare for presentation", LocalDate.of(2023, 04, 24)));
-        */
         List<Task> tasks = taskRepository.findAll();
         model.addAttribute("tasks", tasks);
         return "index";
+    }
+    
+    @RequestMapping("/tasks/{id}")
+    public String closeTask(@PathVariable int id) {
+        taskRepository.deleteById(id);
+        return "redirect:/";
+    }
+    
+    @RequestMapping("/newTask")
+    public String getTaskForm() {
+        return "task-form";
+    }
+    
+    @PostMapping("processForm")
+    public String addTask(@ModelAttribute Task task) {
+        taskRepository.save(task);
+        return "redirect:/";
     }
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -8,7 +8,7 @@
  */
 
 insert into task (id, priority, description, date) values
-(1, 2, 'Pay bills', '2023-04-30'),
-(2, 3, 'Call Kychelle', '2023-04-18'),
-(3, 1, 'Develop new feature in my app', '2023-05-31'),
-(4, 2, 'Prepare for presentation', '2023-04-24');
+(101, 2, 'Pay bills', '2023-04-30'),
+(102, 3, 'Call Kychelle', '2023-04-18'),
+(103, 1, 'Develop new feature in my app', '2023-05-31'),
+(104, 2, 'Prepare for presentation', '2023-04-24');
